1223473526ec9879d63dc7eb97f5a68e902384a0
bob <diksha.yadav022@gmail.com>
2022-11-08 04:10:52 +0530

    project initilisation

---
+++ app.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: "diksha", 
+    lname: "testing", 
+    printFullName : function(){
+        return this.name + " "  + this.lname ;
+    }
+}
---
+++ index.js
@@ -0,0 +1,70 @@
+// to create a server in nodejs
+const http = require('http');
+
+const serve = (req, res) => {
+    res.write("<h1>development rocks here!</h1>");
+    res.end();
+}
+http.createServer(serve).listen(3500);
+
+
+
+
+// const data = [10, 20, 30, 50, 70, 110, 130];
+
+// const getElement = () => 
+// {
+//     let val = document.getElementById("element").value;
+//     alert(data[val]);
+// }
+
+
+
+
+// const fs = require('fs');
+// console.log("-->>", __filename);
+// console.log("->>", __dirname);
+
+
+// const data = [{name: "testing", price: 1200000}, 
+// {name: "testing1", price: 2500000}, 
+// {name: "testing2", price: 1800000}, 
+// {name: "testing3", price: 1900000}, 
+// {name: "testing4", price: 1000000000}
+
+//     return item.price > 1900000
+// const filteredData = data.filter((item)=> {
+    // })
+    
+    // console.log(filteredData);
+    
+// const fs = require('fs');)
+// fs.writeFileSync('newfile.txt', "new data");
+
+
+
+
+
+
+// const app = require('./app');
+// console.log(app.printFullName());
+
+// let a = 10;
+// let b = 20; 
+// let c = 30;
+// console.log(a + b + c);
+
+// for(let i=0; i<=10; i++)
+// {
+//     console.log(i);
+// }
+
+// var a = prompt("Enter a number");
+// var b = prompt("enter another number");
+// if(a > b){
+//     console.log(`${a} a is greater than b`);
+// }
+// else 
+// {
+//     console.log(`${b} b  is greater than a`);
+// }
